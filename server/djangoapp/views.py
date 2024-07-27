115da0d9c2659e146d4f4c9d108b55e6499c8a1d
carlos-vf <carlosvelazquezf@hotmail.com>
2024-07-27 15:06:54 +0200

    Improved code readability

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -76,7 +76,7 @@ def registration(request):
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
-    else :
+    else:
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
@@ -123,10 +123,11 @@ def add_review(request):
             post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            return JsonResponse({"status": 401,
+                                 "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
-    
+
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
@@ -136,5 +137,6 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name,
+                     "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
